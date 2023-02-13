bd1d64e64846bb94a371f9127d00a63e2e40ba17
tiansha <454412129@qq.com>
2023-02-13 15:42:48 +0800

    add tw-element and update navbar

--- app/Header.tsx
+++ app/Header.tsx
@@ -3,12 +3,43 @@ import React from 'react'
 
 function Header() {
   return (
-    <header className="p-5 bg-blue-500">
-        <Link href="/" className="px-2 py-1 bg-white text-blue-500 rounded-lg">Home</Link>
-        <Link href="/todos" className="ml-2 px-2 py-1 bg-white text-blue-500 rounded-lg">Todos</Link>
-        <Link href="/search" className="ml-2 px-2 py-1 bg-white text-blue-500 rounded-lg">Search</Link>
-        <Link href="/auth" className="ml-2 px-2 py-1 bg-white text-blue-500 rounded-lg">Auth</Link>
-    </header>
+
+    <nav className="
+      relative
+      w-full
+      flex flex-wrap
+      items-center
+      justify-between
+      py-2
+      bg-gray-100
+      text-gray-500
+      hover:text-gray-700
+      focus:text-gray-700
+      shadow-lg
+      navbar navbar-expand-lg navbar-light
+      ">
+        <div className="container-fluid w-full flex flex-wrap items-center justify-between px-6">
+        <div className="collapse navbar-collapse flex-grow items-center" id="navbarSupportedContent">
+
+        <ul className="navbar-nav flex flex-col pl-0 list-style-none mr-auto">
+          <li className="nav-item p-2">
+            <Link className="nav-link text-gray-500 hover:text-gray-700 focus:text-gray-700 p-0" href="/">Home</Link>
+          </li>
+          <li className="nav-item p-2">
+            <Link className="nav-link text-gray-500 hover:text-gray-700 focus:text-gray-700 p-0" href="/todos">Todos</Link>
+          </li>
+          <li className="nav-item p-2">
+            <Link className="nav-link text-gray-500 hover:text-gray-700 focus:text-gray-700 p-0" href="/search">Search</Link>
+          </li>
+          <li className="nav-item p-2">
+            <Link className="nav-link text-gray-500 hover:text-gray-700 focus:text-gray-700 p-0" href="/auth">Login</Link>
+          </li>
+        </ul>
+
+      </div>
+      </div>
+    </nav>
+
   )
 }
 
